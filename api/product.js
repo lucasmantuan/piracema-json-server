db81cde95e32d6ccbd2efb219f6b381042224b9c
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-06 23:40:30 -0300

    Json Server com Express

--- api/product.js
+++ api/product.js
@@ -7,15 +7,15 @@ const router = express.Router();
  * @return product list | empty.
  */
 router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
+    try {
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+var express = require('express');
+
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+
+var server = express();
+server.use('/api', jsonServer.defaults()); 
+server.use('/api', jsonServer.router('db.json'));
+
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
-
+// server.use(middlewares);
+// server.use(router);
 server.listen(port);
