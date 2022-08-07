2dbe7284417bdcc5dd32efc7063a7a2b16d1368d
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 14:46:02 -0300

    Alterações Diversas

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-const jsonServer = require("json-server");
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 5000;
+
+// server.use(
+//     cors({
+//         origin: true,
+//         credentials: true,
+//         preflightContinue: false,
+//         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     })
+// );
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
+
+
+
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
 
-const cors = require("cors");
+router.render = (req, res) => {
 
-server.use(
-    cors({
-        origin: true,
-        credentials: true,
-        preflightContinue: false,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    })
-);
+    let headersToExpose = ['Authorization'];
+    let currentExposedHeaders = res.getHeader('Access-Control-Expose-Headers');
 
-server.options("*", cors());
+    if (currentExposedHeaders) {
+        res.header('Access-Control-Expose-Headers', headersToExpose.concat(currentExposedHeaders.split(',')).join(','))
+    } else {
+        res.header('Access-Control-Expose-Headers', headersToExpose.join(','));
+    }
+
+    res.send(res.locals.data)
+}
+
+//default json-server middlewares
 server.use(middlewares);
+
+server.use((req, res, next) => {
+    res.header('Authorization', 'Bearer 2ee636c8-06b6-474b-b562-f1cde69ea575');
+    next();
+});
+
 server.use(router);
 
-server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
+server.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
