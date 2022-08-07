b33937876ff038db3f97952c12d180cbaf5578b8
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 13:57:12 -0300

    Teste de Erro

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const server = jsonserver.create();
 const router = jsonserver.router("db.json");
 const middlewares = jsonserver.defaults();
 const port = process.env.PORT || 5000;
+
+router.render = (req, res) => {
+    res.status(500).jsonp({
+        error: "error message here"
+    })
+}
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => console.log("http://localhost:" + port));
