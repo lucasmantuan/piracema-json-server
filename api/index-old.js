95e00d3cd8995d07b1c9ed33d25785aa23c4b0fa
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 00:36:48 -0300

    Alterações Diversas

--- api/index-old.js
+++ api/index-old.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+const product = require("./product");
 
 app.use(express.json({ extended: false }));
 
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var jsonServer = require("json-server");
-// var db = require("./db.js");
-var server = jsonServer.create();
-var router = jsonServer.router("db.json");
-var middlewares = jsonServer.defaults();
-var port = process.env.PORT || 5000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(port, function () {
-    console.log("JSON Server is running on http://localhost:" + port);
-});
+server.listen(port);
