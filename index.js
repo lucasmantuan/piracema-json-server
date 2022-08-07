2d8e7c9d77a7e4c0b3388a5bb5aa6b69f6abc0ff
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 14:27:01 -0300

    Adicionado o Pacote Cors

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const jsonserver = require("json-server");
-const server = jsonserver.create();
-const router = jsonserver.router("db.json");
-const middlewares = jsonserver.defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
-router.render = (req, res) => {
-    res.status(500).jsonp({
-        error: "error message here"
-    })
-}
+const cors = require("cors");
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => console.log("http://localhost:" + port));
+
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
+server.options("*", cors());
+
+server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
