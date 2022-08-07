b2b0608401c69241145b97acf203c0b17cee87f1
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 15:00:18 -0300

    Alteração Headers

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
 
 router.render = (req, res) => {
 
@@ -49,6 +50,58 @@ server.use((req, res, next) => {
 
 server.use(router);
 
-server.listen(3000, () => {
-    console.log('http://localhost:3000');
-});
+server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
+
+
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 5000;
+
+// server.use(
+//     cors({
+//         origin: true,
+//         credentials: true,
+//         preflightContinue: false,
+//         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     })
+// );
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
+
+
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 5000;
+
+// router.render = (req, res) => {
+
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     // another common pattern
+//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
+// }
+
+// //default json-server middlewares
+// server.use(middlewares);
+
+// // server.use((req, res, next) => {
+// //     res.header('Authorization', 'Bearer 2ee636c8-06b6-474b-b562-f1cde69ea575');
+// //     next();
+// // });
+
+// server.use(router);
+
+// server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
