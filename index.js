c8f7174cea61767a47dd8959f7a9d54daf6fb687
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 15:31:55 -0300

    Alterações no Header

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 5000;
-
-// server.use(
-//     cors({
-//         origin: true,
-//         credentials: true,
-//         preflightContinue: false,
-//         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     })
-// );
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
-
-
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    res.setHeader("Access-Control-Allow-Credentials", true)
+    // res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
     next();
 });
 
