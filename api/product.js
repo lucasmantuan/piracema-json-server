81effce6acd988387eb8a73d0539edf8e099a6ee
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-07 00:00:52 -0300

    Alterações Diversas

--- api/product.js
+++ api/product.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+// const server = express();
+const jsonserver = require('json-server');
+
 /**
  * GET product list.
  *
  * @return product list | empty.
  */
-router.get("/", async (req, res) => {
-    try {
-        res.json({
-            status: 200,
-            message: "Get data has successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Server error");
-    }
-});
+router.use('/', jsonserver.defaults());
+router.use('/', jsonserver.router('db.json'));
+
+// router.get("/", () => {
+
+//     // try {
+//     //     res.json({
+//     //         status: 200,
+//     //         message: "Get data has successfully",
+//     //     });
+//     // } catch (error) {
+//     //     console.error(error);
+//     //     return res.status(500).send("Server error");
+//     // }
+// });
 
 module.exports = router;
