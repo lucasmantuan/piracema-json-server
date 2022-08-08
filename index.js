d3ce077dcc9c5d44383f57703f198d78e28b0648
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-08 00:02:58 -0300

    Alterações Diversas

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ noCors: true });
 const port = process.env.PORT || 5000;
-
-server.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Credentials", true)
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
-    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
-    next();
-});
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => console.log("JSON Server is running in http://localhost:" + port));
+server.listen(port, () => console.log("JSON Server is Running in http://localhost:" + port));
