e2977297ea1e82b391c402f3aab3d313fca46225
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-09 16:21:22 -0300

    Adicionada Novas Configurações para a Vercel

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => console.log("JSON Server is running"));
+
+module.exports = server;
