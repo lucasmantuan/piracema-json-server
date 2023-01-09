315c36e044a0b06f00ea95baf57c2daa819b6cd4
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-09 16:53:22 -0300

    Ajuste nas Rotas e Porta

--- server.js
+++ server.js
@@ -3,19 +3,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
 const port = process.env.PORT || 3000;
 
 server.use(middlewares)
-
-server.use(jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id"
-}))
-
+server.use(jsonServer.rewriter({ "/api/antenas": "/antenas", "/api/peixes": "/peixes", "/api/passagens": "/passagens" }))
 server.use(router)
-server.listen(port, () => {
-    console.log("JSON Server is running")
-})
+server.listen(port, () => { console.log("JSON Server is running") })
 
 module.exports = server
