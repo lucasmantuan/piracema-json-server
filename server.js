bbcdcda5800c4824cb26ef672656a13e4f3389e4
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-09 16:36:03 -0300

    Alterada as Configurações para a Vercel

---
+++ server.js
@@ -0,0 +1,21 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares)
+
+server.use(jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id"
+}))
+
+server.use(router)
+server.listen(port, () => {
+    console.log("JSON Server is running")
+})
+
+module.exports = server
